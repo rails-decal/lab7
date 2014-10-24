88bcd88b989be59120179b50fc5f06a066065bdf
jaytlin <heyjaylin@gmail.com>
2014-10-23 17:58:01 -0700

    User test.

--- test/models/pokemon_test.rb
+++ test/models/pokemon_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PokemonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without name" do
+    pokemon = Pokemon.new
+    assert_not pokemon.save, "Saved a Pokemon without a name"
+  end
+  test "should not save if name not unique" do
+      pokemon = Pokemon.new(name: "Pokemon 1", level: 1, user_id: 1)
+      assert_not pokemon.save, "Saved a Pokemon with non-unique title"
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without name" do
+    user = User.new
+    assert_not user.save, "Saved a User without a name"
+  end
+  test "should not save if name not unique" do
+      user = User.new(name: "Ash")
+      assert_not user.save, "Saved a User with non-unique title"
+  end
 end
